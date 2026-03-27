07b25996266617809c16fded62b6b35fb472904a
Blanola Dsouza <dsouzablanola@gmail.com>
2026-03-27 22:39:58 +0530

    Area_of_rectangle

---
+++ Area_of_rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int length=45;
+    int breadth=25;
+    int area;
+    area=length*breadth;
+    printf("Length is: %d\n",length);
+    printf("Breadth is: %d\n",breadth);
+    printf("Area of the rectangle is: %d",area);
+    return 0;
+}
