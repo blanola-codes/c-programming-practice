6012887c855352a2608f05565c59bb8e28b5f613
Blanola Dsouza <dsouzablanola@gmail.com>
2026-03-27 22:05:25 +0530

    Escape_sequences

---
+++ Escape_sequences.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    printf("I'm learning C.\nAnd it's awesome!\n");
+    printf("Name \tAge\n");
+    printf("Blanola\t19\n");
+    printf("C:\\Users\\Student\n");
+    printf("\"Programming is fun\"");
+    return 0;
+}
+
