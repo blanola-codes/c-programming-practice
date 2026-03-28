990b52b6a589b3c3deea8535408cd0ca174e179a
Blanola Dsouza <dsouzablanola@gmail.com>
2026-03-28 19:15:58 +0530

    Student_report_card

---
+++ Student_report_card.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+int studentID=31;
+float score=89.5;
+char grade='A';
+
+printf("Student ID is:%d\n",studentID);
+printf("Score is:%.1f\n",score);
+printf("Grade is:%c\n",grade);
+  return 0;
+}
